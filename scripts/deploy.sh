66672b9e701b1d4d2b08e0052a234af1f5f5a7fc
jg75 <james@amberengine.com>
2019-08-01 16:31:50 -0400

    added some html and deployment placeholders

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
+ship-it() {
+    local bucket=$1
+    local distribution=$2
+
+    echo aws s3 sync www s3://$bucket/www --delete
+
+    echo aws cloudfront \
+        create-invalidation \
+        --distribution-id $distribution \
+        --paths '/www/*'
+
+    echo sam deploy something or other
+}
+
 case $CODEBUILD_INITIATOR in
-    codepipeline/squrl)
-        echo do epic shit
-        ;;
-    *)
-        echo do epic shit in a default manner
+    codepipeline/squrl) ship-it www.squrl.com blahblah
         ;;
 esac
