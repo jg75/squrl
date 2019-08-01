bbdc021426834ec735bc263626546e17898b07bb
jg75 <james@amberengine.com>
2019-08-01 17:52:56 -0400

    update sam deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,36 @@
 #! /bin/bash
+S3_DEPLOY_BUCKET=ae-sam-deploy
 
 ship-it() {
-    local bucket=$1
-    local distribution=$2
+    local api_version=$1
+    local bucket=$2
+    local distribution_id=$3
+
+    sam build \
+        --parameter-overrides \
+            "ParameterKey=ApiVersionParameter,ParameterValue=$api_version
+            ParameterKey=S3BucketParameter,ParameterValue=$bucket"
+
+    sam package \
+        --output-template-file sam-packaged.yml \
+        --s3-bucket $S3_DEPLOY_BUCKET
+
+    sam deploy \
+        --capabilities CAPABILITY_IAM \
+        --template-file sam-packaged.yml \
+        --stack-name squrl-$api_version
 
     echo aws s3 sync www s3://$bucket/www --delete
 
     echo aws cloudfront \
         create-invalidation \
-        --distribution-id $distribution \
+        --distribution-id $distribution_id \
         --paths '/www/*'
-
-    echo sam deploy something or other
 }
 
 case $CODEBUILD_INITIATOR in
-    codepipeline/squrl) ship-it www.squrl.com blahblah
+    codepipeline/squrl) ship-it v1 www.squrl.com distribution
+        ;;
+    *) ship-it v1 www.squrl.com distribution
         ;;
 esac
